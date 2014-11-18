4d51ccc065b06a4d124d133b08370215fa01193f
JamesPiechota <piechota@gmail.com>
2014-11-17 18:47:26 -0800

    Update ipn.php

--- ipn.php
+++ ipn.php
@@ -11,36 +11,36 @@
 //     the product
     
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-		    // ipn should only be POSTed
+    // ipn should only be POSTed
     http_response_code(400);
 } else {
-	    include("authentication.php");
-	    $data = json_decode($HTTP_RAW_POST_DATA);
-			    $invoice = safe($data->{"id"});
-					    $status = safe($data->{"status"});
-									    $signature = safe($_SERVER["HTTP_API_SIGN"]);
-													    $nonce = safe($_SERVER["HTTP_API_NONCE"]);
-										    $private_key = getenv("YELLOW_SECRET");
-														    $url = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-														
-															    $test_signature = get_signature($private_key,
-															                                    $url,
-															                                    $nonce,
-															                                    $HTTP_RAW_POST_DATA);
-																																																			
-																																																											    if ($signature != $test_signature) {
+    include("authentication.php");
+    $data = json_decode($HTTP_RAW_POST_DATA);
+    $invoice = safe($data->{"id"});
+    $status = safe($data->{"status"});
+    $signature = safe($_SERVER["HTTP_API_SIGN"]);
+    $nonce = safe($_SERVER["HTTP_API_NONCE"]);
+    $private_key = getenv("YELLOW_SECRET");
+    $url = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+    $test_signature = get_signature($private_key,
+                                    $url,
+                                    $nonce,
+                                    $HTTP_RAW_POST_DATA);
+
+    if ($signature != $test_signature) {
         // If signatures are not the same, that means it could be a malicious
         // request: reject it. 
-																																																											    	    http_response_code(403);
-																																																															    }
-									
-	    if ("authorizing" == $status) {
-	    	    error_log("Order is 'pending confirmation', redirecting customer to order complete page.");
-					    } else if ("paid" == $status) {
-					    	    error_log("Order is 'complete', shipping product to customer.");
-									    } else {
-									    	    http_response_code(400);
-													    }
+        http_response_code(403);
+    }
+
+    if ("authorizing" == $status) {
+        error_log("Order is 'pending confirmation', redirecting customer to order complete page.");
+    } else if ("paid" == $status) {
+        error_log("Order is 'complete', shipping product to customer.");
+    } else {
+        http_response_code(400);
+    }
 }
 
 ?>
